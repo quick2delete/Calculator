7d1818767ce5719af428349ea264ee874772cfa8
Hae Lee <haeyounglee212@gmail.com>
2022-08-22 23:33:34 -0400

    'update'

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
 const btns = document.querySelectorAll(".btns");
 const screen = document.querySelector(".screen");
+const equalBtn = document.querySelector(".equal-btn");
 
-const numbers = [];
+let numArray = [];
 
 // Math Functions
-const add = function (a, b) {
-  return a + b;
-};
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
-const subtract = function (a, b) {
-  return a - b;
-};
-
-const multiply = function (a, b) {
-  return a * b;
-};
-
-const divide = function (a, b) {
-  return a / b;
-};
-
-const operate = function (str, a, b) {
+const operate = (str, a, b) => {
   if (str === "+") {
     return add(a, b);
-  } else if (str === "-") {
+  }
+  if (str === "-") {
     return subtract(a, b);
-    54;
-  } else if (str === "*") {
+  }
+  if (str === "*") {
     return multiply(a, b);
-  } else if (str === "/") {
+  }
+  if (str === "/") {
     return divide(a, b);
   }
 };
 
-const showNumbers = function (value) {
-  const div = document.createElement("div");
-  div.classList.add("numbers");
-  div.innerHTML = `<P>${value}</p>`;
-  screen.appendChild(div);
+const showNumbers = (data) => {
+  screen.textContent += data;
   screen.style.fontSize = "2.5rem";
+
+  numArray.push(screen.textContent);
+  const poppedStr = numArray.pop();
+
+  console.log(poppedStr, typeof poppedStr);
+
+  const numStr = poppedStr.split(/[+*\/-]/);
+  // const newArray = new Array(numStr);
+  console.log(numStr);
+  // console.log(screen.textContent);
+
+  // numArray.shift();
+  // console.log(numArray);
 };
+
+const calNumbers = (array) => {
+  console.log(numArray);
+};
+
+calNumbers();
 
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    console.log(e.target.innerText);
-    const data = e.target.innerText;
+    let data = btn.textContent;
     showNumbers(data);
   });
+});
+
+equalBtn.addEventListener("click", () => {
+  console.log("clicked!");
 });
